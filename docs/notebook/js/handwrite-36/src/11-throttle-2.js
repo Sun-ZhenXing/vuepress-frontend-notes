fe1a35bfb8e746c4a7cb05c5bb6203fe952a00b7
summer <3244742300@qq.com>
2023-03-08 19:47:32 +0800

    update: throttle-2

--- docs/notebook/js/handwrite-36/src/11-throttle-2.js
+++ docs/notebook/js/handwrite-36/src/11-throttle-2.js
@@ -1,49 +1,17 @@
 /**
  * @param {Function} func
  * @param {number} wait
- * @param {{leading?: boolean, trailing?: boolean}?} options
  */
-function throttle(func, wait, options) {
-  var timeout, context, args;
-  var previous = 0;
-  if (!options)
-    options = {};
-  if (!options.leading)
-    options.leading = true
-
-  var later = function () {
-    previous = !options.leading ? 0 : new Date().getTime();
-    timeout = null;
-    func.apply(context, args);
-    if (!timeout)
-      context = args = null;
-  };
-
-  var throttled = function () {
-    var now = new Date().getTime();
-    if (!previous && !options.leading)
-      previous = now;
-    var remaining = wait - (now - previous);
-    context = this;
-    args = arguments;
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      previous = now;
-      func.apply(context, args);
-      if (!timeout)
-        context = args = null;
-    } else if (!timeout && options.trailing) {
-      timeout = setTimeout(later, remaining);
-    }
-  };
-
-  throttled.cancel = function () {
-    clearTimeout(timeout);
-    previous = 0;
-    timeout = null;
-  };
-  return throttled;
+function throttle(func, wait) {
+  // 采用定时器
+  let timer = null
+  return function () {
+    let conetxt = this
+    let args = arguments
+    if (timer) return
+    timer = setTimeout(function () {
+      func.call(conetxt, args)
+      timer = null
+    },wait)
+  }
 }
