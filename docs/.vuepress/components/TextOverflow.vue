702436f968e02145a58d55df7c67e93e605e709d
Sun-ZhenXing <1006925066@qq.com>
2023-04-12 19:01:24 +0800

    docs(notebook): add css/font-family & css/text-overflow

---
+++ docs/.vuepress/components/TextOverflow.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="note">
+    <div class="note-1">
+      好想做伊雷娜小姐的狗啊！
+    </div>
+    <div class="note-2">
+      好想做伊雷娜小姐的狗啊！
+    </div>
+    <div class="note-3">
+      好想做伊雷娜小姐的狗啊！
+    </div>
+    <div class="note-4">
+      好想做伊雷娜小姐的狗啊！
+    </div>
+    <div class="note5">
+      好想做伊雷娜小姐的狗啊！
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.note {
+  border: 1px solid #ccc;
+
+  >div {
+    overflow: hidden;
+    white-space: nowrap;
+    width: 80px;
+    padding: 1rem;
+  }
+
+  &-1 {
+    width: 100% !important;
+  }
+
+  &-2 {
+    text-overflow: clip;
+  }
+
+  &-3 {
+    text-overflow: ellipsis;
+  }
+
+  &-4 {
+    text-overflow: "###";
+  }
+
+  &-5 {
+    text-overflow: fade(10px);
+  }
+}
+</style>
